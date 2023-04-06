bba922ea50ff05aa9b35947b107b5fe703825f75
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-06 17:06:03 -0400

    create homepage

--- Osu!Mates/src/Pages/Home/HomePage.jsx
+++ Osu!Mates/src/Pages/Home/HomePage.jsx
@@ -1,7 +1,11 @@
+import logo from '../../../public/logo.png';
+import './HomePage.css'
 export default function HomePage() {
     return (
-        <div>
-            Homepage
+        <div className='homePage'>
+            <h1>Welcome to osu!Mate Creator!</h1>
+            <h3>A place to create your very own set of osu!Mates to play multi mode with</h3>
+            <img src={logo}/>
         </div>
     )
 }
