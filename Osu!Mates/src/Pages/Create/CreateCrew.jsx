9e3b9a70d0efd9fd51f4125ce59b44e2680d3f24
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-06 17:58:33 -0400

    add createCrew form

--- Osu!Mates/src/Pages/Create/CreateCrew.jsx
+++ Osu!Mates/src/Pages/Create/CreateCrew.jsx
@@ -1,7 +1,52 @@
+import './CreateCrew.css'
+
 export default function CreateCrew() {
     return (
-        <div>
-            createCrew
+        <div className="createCrew">
+            <h1>Create a New Crewmate</h1>
+            <img src='../../../public/create.webp' />
+
+            <div className="form">
+                <form className='create-crew-form'>
+                    <label>
+                        Name:
+                        <input
+                        type="text"
+                        placeholder='funnyBunny'
+                    />
+                    </label>
+                    
+                    <label>
+                        Country Ranking:
+                        <input 
+                        type="text"
+                        placeholder='245,823'
+                    />
+                    </label>
+                    
+                    <label>
+                        Hit accuracy (%):
+                        <input 
+                        type="number"
+                        placeholder='95.56'
+                    />
+                    </label>
+                   
+                    <label>
+                        Crew color:
+                        <select>
+                            <option value="null">-select-</option>
+                            <option value="blue">blue</option>
+                            <option value="purple">purple</option>
+                            <option value="pink">pink</option>
+                            <option value="green">green</option>
+                            <option value="yellow">yellow</option>
+                        </select>
+                    </label>
+                    
+                    <button className='primary-btn'>Create osu!Mate</button>
+                </form>
+            </div>
         </div>
     )
 }
