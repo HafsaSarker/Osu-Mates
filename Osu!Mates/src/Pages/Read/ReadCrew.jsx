6ae78c87bef0ed972938bb331ba29e5a7c23d501
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-04-06 18:16:46 -0400

    add gallery page

--- Osu!Mates/src/Pages/Read/ReadCrew.jsx
+++ Osu!Mates/src/Pages/Read/ReadCrew.jsx
@@ -1,9 +1,14 @@
 import './ReadCrew.css';
-
+import { Link } from 'react-router-dom';
 export default function ReadCrew() {
     return (
         <div className="gallery">
-            gallery
+            <h1>Your osu!Mates Gallery:</h1>
+            <h4>You haven't made an osu!Mate yet!</h4>
+            <Link to="/create">
+                <button className='primary-btn'>Create one here!</button>
+            </Link>
+            
         </div>
     )
 }
